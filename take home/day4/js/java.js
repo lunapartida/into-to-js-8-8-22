b6f9f375dcec8344e3f68219923c8e0b9da198d8
Luna Partida <lunapartida93@icloud.com>
2022-08-27 17:21:49 -0700

    day 5 and 6

--- take home/day4/js/java.js
+++ take home/day4/js/java.js
@@ -24,6 +24,5 @@ comparison();
 
 // ------------ TAKE HOME CHALLENGE ------------
 // Add an edge case to your daily challenge code
-
 // Check if the user input anything other than a number
 // If so, prompt the user again to input a number
